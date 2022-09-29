f54ea6a03ff709030e80b8b34c4e2819d012e312
mohamed <mohammedalbehairy90@gmail.com>
2022-09-29 10:40:46 +0200

    Remove unused imports

--- src/app/modules/kyb/kyb.module.ts
+++ src/app/modules/kyb/kyb.module.ts
@@ -9,13 +9,6 @@ import { Ng2FlatpickrModule } from 'ng2-flatpickr';
 import { NouisliderModule } from 'ng2-nouislider';
 import { CardSnippetModule } from '@core/components/card-snippet/card-snippet.module';
 
-import { BusinessDetailsAddressPageComponent } from './components/business-details-address/business-details-address-page.component';
-import { BusinessDetailsFinancialPageComponent } from './components/business-details-financial/business-details-financial-page.component';
-import { BusinessDetailsOtherPageComponent } from './components/business-details-other/business-details-other-page.component';
-import { BusinessDetailsOther2PageComponent } from './components/business-details-other2/business-details-other2-page.component';
-import { BusinessDocumentUploadPageComponent } from './components/business-document-upload/business-document-upload-page.component';
-import { BusinessDetailsInfoPageComponent } from './components/business-details-info/business-details-info-page.component';
-
 import { KybRoutingModule } from './kyb-routing.module';
 import { stepperModule } from '../shared/stepper/stepper.module';
 import { AddressComponent } from './components/address/address.component';
@@ -27,12 +20,6 @@ import { OtherInfoComponent } from './components/other-info/other-info.component
 
 @NgModule({
   declarations: [
-    BusinessDetailsInfoPageComponent,
-    BusinessDetailsAddressPageComponent,
-    BusinessDetailsFinancialPageComponent,
-    BusinessDetailsOtherPageComponent,
-    BusinessDetailsOther2PageComponent,
-    BusinessDocumentUploadPageComponent,
     AddressComponent,
     FinancialComponent,
     InfoComponent,
