b163827141ecc1358fd4530c67a422447b240060
mohamed <mohammedalbehairy90@gmail.com>
2022-09-29 16:36:10 +0200

    Add ecommerce step

--- src/app/modules/kyb/components/e-commerce/e-commerce.component.ts
+++ src/app/modules/kyb/components/e-commerce/e-commerce.component.ts
@@ -1,51 +1,82 @@
-import { Component, Input, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
-import { FlatpickrOptions } from "ng2-flatpickr";
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { KybService } from '../../services/kyb.service';
 
 @Component({
   selector: 'app-e-commerce',
   templateUrl: './e-commerce.component.html',
-  styleUrls: ['./e-commerce.component.scss']
+  styleUrls: ['./e-commerce.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class ECommerceComponent implements OnInit {
+  public ecommerceForm: UntypedFormGroup;
+  public submitted = false;
+  public loading = false;
 
- 
-
-  public TDNameVar;
-  public TDEmailVar;
-  public selectBasicLoading = false;
-  basicDPdata;
-  public data: any;
-
-  public selectBasic = [
-    { name: 'UK' },
-    { name: 'USA' },
-    { name: 'Spain' },
-    { name: 'France' },
-    { name: 'Italy' },
-    { name: 'Australia' }
-  ];
-
-
-  public basicDateOptions: FlatpickrOptions = {
-    altInput: true
-  };
-
-  public birthDateOptions: FlatpickrOptions = {
-    altInput: true
-  };
   constructor(
     private _router: Router,
-  ) {
-
-  }
+    private formBuilder: UntypedFormBuilder,
+    private _kybService: KybService
+  ) {}
   ngOnInit(): void {
+    this.initForm();
+
+    this.loadData();
+  }
+
+  initForm() {
+    // Reactive form initialization
+    this.ecommerceForm = this.formBuilder.group({
+      averageMonthlyValueOfInventoryHeld: [null, Validators.required],
+      currentRating: [null, Validators.required],
+    });
+  }
+
+  loadData() {}
+
+  onSubmit() {
+    console.log('=-=-=-=-=-=-=--', this.ecommerceForm);
+
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.ecommerceForm.invalid) {
+      return;
+    }
+    this.loading = true;
+    let data = this.getCleanValue();
+
+    this._kybService.setData(data).subscribe(
+      (res) => {
+        console.log(res);
+
+        this._router.navigate(['kyb/uploaddocs']);
+      },
+      (err) => {
+        this.loading = false;
+        console.log(err);
+      }
+    );
+  }
+
+  getCleanValue() {
+    return {
+      financialInformation: {
+        ...this.ecommerceForm.value,
+      },
+    };
+  }
+
+  get f() {
+    return this.ecommerceForm.controls;
   }
 
   back() {
     this._router.navigate(['kyb/otherinfo']);
-  }
-  next() {
-    this._router.navigate(['kyb/uploaddocs']);
   }
 }
