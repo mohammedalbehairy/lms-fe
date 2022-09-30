b2001adf4bd743a3bd602fa76c127b352336fcce
mohamed <mohammedalbehairy90@gmail.com>
2022-09-30 02:40:23 +0200

    Add docs upload logic

--- src/app/modules/kyb/components/document-upload/document-upload.component.ts
+++ src/app/modules/kyb/components/document-upload/document-upload.component.ts
@@ -1,50 +1,135 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  ViewEncapsulation,
+  ChangeDetectorRef,
+} from '@angular/core';
+import {
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { FlatpickrOptions } from 'ng2-flatpickr';
+import moment from 'moment';
+import { KybService } from '../../services/kyb.service';
 
 @Component({
   selector: 'app-document-upload',
   templateUrl: './document-upload.component.html',
   styleUrls: ['./document-upload.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class DocumentUploadComponent implements OnInit {
-  public TDNameVar;
-  public TDEmailVar;
-  public selectBasicLoading = false;
-  basicDPdata;
-  public data: any;
-
-  public selectBasic = [
-    { name: 'UK' },
-    { name: 'USA' },
-    { name: 'Spain' },
-    { name: 'France' },
-    { name: 'Italy' },
-    { name: 'Australia' },
-  ];
-
-  public basicDateOptions: FlatpickrOptions = {
+  public tradeLicenseExpiryDateOptions: any = {
     altInput: true,
+    defaultDate: ['2010-05-23'],
+    altFormat: 'j/m/Y',
   };
-
-  public birthDateOptions: FlatpickrOptions = {
+  public expiryDate: any = {
     altInput: true,
+    defaultDate: ['2010-05-23'],
+    altFormat: 'j/m/Y',
   };
-  constructor(private _router: Router, private modalService: NgbModal) {}
-  ngOnInit(): void {}
+  public docsForm: UntypedFormGroup;
+  public submitted = false;
+  public loading = false;
+  public image1 = '';
+
+  constructor(
+    private _router: Router,
+    private formBuilder: UntypedFormBuilder,
+    private _kybService: KybService
+  ) {}
+  ngOnInit(): void {
+    this.initForm();
+
+    this.loadData();
+  }
+
+  initForm() {
+    // Reactive form initialization
+    this.docsForm = this.formBuilder.group({
+      tradeLicenseFile: [null, Validators.required],
+      tradeLicenseExpiryDate: [null, Validators.required],
+      moa: [null, Validators.required],
+      bankStatementFromTo: [null, Validators.required],
+      ejariOrLeaseAgreement: [null, Validators.required],
+      expiryDate: [null, Validators.required],
+      vatForm: [null, Validators.required],
+    });
+  }
+
+  loadData() {}
+
+  onSubmit() {
+    console.log('=-=-=-=-=-=-=--', this.docsForm);
+
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.docsForm.invalid) {
+      return;
+    }
+    this.loading = true;
+    let data = this.getCleanValue();
+
+    this._kybService.setData(data).subscribe(
+      (res) => {
+        console.log(res);
+
+        this._router.navigate(['kyc/personalinfo']);
+      },
+      (err) => {
+        this.loading = false;
+        console.log(err);
+      }
+    );
+  }
+
+  getCleanValue() {
+    return {
+      businessDocument: {
+        ...this.docsForm.value,
+        tradeLicenseExpiryDate: moment(
+          this.f.tradeLicenseExpiryDate.value[0]
+        ).format('DD MMMM YYYY'),
+        expiryDate: moment(this.f.expiryDate.value[0]).format('DD MMMM YYYY'),
+      },
+    };
+  }
+
+  /**
+   * Upload Image
+   *
+   * @param event
+   */
+  uploadFile(event: any) {
+    let inputId = event.target.id;
+    let data = null;
+    console.log('=====uploadFile=========inputId==============', inputId);
+
+    if (event.target.files && event.target.files[0]) {
+      let reader = new FileReader();
+
+      reader.onload = (event: any) => {
+        console.log(
+          '=====uploadFile===onload======inputId========event======',
+          event
+        );
+
+        data = event.target.result;
+        this.f[inputId].setValue(data);
+      };
+      reader.readAsDataURL(event.target.files[0]);
+    }
+  }
+
+  get f() {
+    return this.docsForm.controls;
+  }
 
   back() {
-    this._router.navigate(['kyb/otherinfo2']);
-  }
-  next() {
-    this._router.navigate(['kyc/personalinfo']);
-  }
-
-  modalOpenDefault(modalDefault) {
-    this.modalService.open(modalDefault, {
-      centered: true,
-      size: 'lg',
-    });
+    this._router.navigate(['kyb/ecommerce']);
   }
 }
