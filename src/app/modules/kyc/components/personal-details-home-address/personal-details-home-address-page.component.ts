73b0eb4278a8b9e2c8baa966c896d3d725469bd8
mohamed <mohammedalbehairy90@gmail.com>
2022-10-01 22:16:24 +0200

    Add personal home address

--- src/app/modules/kyc/components/personal-details-home-address/personal-details-home-address-page.component.ts
+++ src/app/modules/kyc/components/personal-details-home-address/personal-details-home-address-page.component.ts
@@ -1,54 +1,86 @@
-import { Component, Input, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
-import { FlatpickrOptions } from "ng2-flatpickr";
-
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import { KycService } from '../../services/kyc.service';
 
 @Component({
   selector: 'app-personal-details-home-address-page',
   templateUrl: './personal-details-home-address-page.component.html',
   styleUrls: ['./personal-details-home-address-page.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
-
 export class PersonalDetailsHomeAddressPageComponent implements OnInit {
+  public infoForm: UntypedFormGroup;
+  public submitted = false;
+  public loading = false;
 
-
-  public TDNameVar;
-  public TDEmailVar;
-  public selectBasicLoading = false;
-  basicDPdata;
-  public data: any;
-
-  public selectBasic = [
-    { name: 'UK' },
-    { name: 'USA' },
-    { name: 'Spain' },
-    { name: 'France' },
-    { name: 'Italy' },
-    { name: 'Australia' }
-  ];
-
-
-  public basicDateOptions: FlatpickrOptions = {
-    altInput: true
-  };
-
-  public birthDateOptions: FlatpickrOptions = {
-    altInput: true
-  };
   constructor(
     private _router: Router,
-
-  ) {
-
-  }
+    private formBuilder: UntypedFormBuilder,
+    private _kycService: KycService
+  ) {}
   ngOnInit(): void {
+    this.initForm();
+
+    this.loadData();
+  }
+
+  initForm() {
+    // Reactive form initialization
+    this.infoForm = this.formBuilder.group({
+      addressLine1: [null, Validators.required],
+      addressLine2: [null, Validators.required],
+      city: [null, Validators.required],
+      postCode: [null],
+      country: [null, Validators.required],
+      landLineNumber: [null, Validators.required],
+    });
+  }
+
+  loadData() {}
+
+  onSubmit() {
+    console.log('=-=-=-=-=-=-=--', this.infoForm);
+
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.infoForm.invalid) {
+      return;
+    }
+    this.loading = true;
+    let data = this.getCleanValue();
+
+    this._kycService.setData(data).subscribe(
+      (res) => {
+        console.log(res);
+
+        this._router.navigate(['kyc/uploaddocuments']);
+      },
+      (err) => {
+        this.loading = false;
+        console.log(err);
+      }
+    );
+  }
+
+  getCleanValue() {
+    return {
+      homeAddress: {
+        ...this.infoForm.value,
+      },
+    };
+  }
+
+  get f() {
+    return this.infoForm.controls;
   }
 
   back() {
     this._router.navigate(['kyc/contactdetails']);
-  }
-
-  next() {
-    this._router.navigate(['kyc/uploaddocuments']);
   }
 }
