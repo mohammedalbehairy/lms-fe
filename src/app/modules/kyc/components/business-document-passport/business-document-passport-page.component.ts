e411606a822f7af0942d3d20be2474720234aeb3
mohamed <mohammedalbehairy90@gmail.com>
2022-10-02 00:33:53 +0200

    Add business doc & shareholder steps

--- src/app/modules/kyc/components/business-document-passport/business-document-passport-page.component.ts
+++ src/app/modules/kyc/components/business-document-passport/business-document-passport-page.component.ts
@@ -1,52 +1,147 @@
-import { Component, Input, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
-import { FlatpickrOptions } from "ng2-flatpickr";
-
+import {
+  Component,
+  Input,
+  OnInit,
+  ViewEncapsulation,
+  ChangeDetectorRef,
+} from '@angular/core';
+import {
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import moment from 'moment';
+import { KycService } from '../../services/kyc.service';
 
 @Component({
   selector: 'app-business-document-passport-page',
   templateUrl: './business-document-passport-page.component.html',
   styleUrls: ['./business-document-passport-page.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
-
 export class BusinessDocumentPassportPageComponent implements OnInit {
-
-
-  public TDNameVar;
-  public TDEmailVar;
-  public selectBasicLoading = false;
-  basicDPdata;
-  public data: any;
-
-  public selectBasic = [
-    { name: 'UK' },
-    { name: 'USA' },
-    { name: 'Spain' },
-    { name: 'France' },
-    { name: 'Italy' },
-    { name: 'Australia' }
-  ];
-
-
-  public basicDateOptions: FlatpickrOptions = {
-    altInput: true
+  public passportExpiryDateOptions: any = {
+    altInput: true,
+    defaultDate: ['2010-05-23'],
+    altFormat: 'j/m/Y',
   };
 
-  public birthDateOptions: FlatpickrOptions = {
-    altInput: true
+  public emiratesIdExpiryDateOptions: any = {
+    altInput: true,
+    defaultDate: ['2010-05-23'],
+    altFormat: 'j/m/Y',
   };
+
+  public residenceVisaExpiryDateOptions: any = {
+    altInput: true,
+    defaultDate: ['2010-05-23'],
+    altFormat: 'j/m/Y',
+  };
+
+  public docsForm: UntypedFormGroup;
+  public submitted = false;
+  public loading = false;
+  public image1 = '';
+
   constructor(
     private _router: Router,
-  ) {
-
-  }
+    private formBuilder: UntypedFormBuilder,
+    private _kycService: KycService
+  ) {}
   ngOnInit(): void {
+    this.initForm();
+
+    this.loadData();
+  }
+
+  initForm() {
+    // Reactive form initialization
+    this.docsForm = this.formBuilder.group({
+      passport: [null, Validators.required],
+      passportExpiryDate: [null, Validators.required],
+      emiratesId: [null, Validators.required],
+      emiratesIdExpiryDate: [null, Validators.required],
+      residenceVisa: [null, Validators.required],
+      residenceVisaExpiryDate: [null, Validators.required],
+    });
+  }
+
+  loadData() {}
+
+  onSubmit() {
+    console.log('=-=-=-=-=-=-=--', this.docsForm);
+
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.docsForm.invalid) {
+      return;
+    }
+    this.loading = true;
+    let data = this.getCleanValue();
+
+    this._kycService.setData(data).subscribe(
+      (res) => {
+        console.log(res);
+
+        this._router.navigate(['kyc/addshareholder']);
+      },
+      (err) => {
+        this.loading = false;
+        console.log(err);
+      }
+    );
+  }
+
+  getCleanValue() {
+    return {
+      personalDocument: {
+        ...this.docsForm.value,
+        passportExpiryDate: moment(this.f.passportExpiryDate.value[0]).format(
+          'DD MMMM YYYY'
+        ),
+        emiratesIdExpiryDate: moment(
+          this.f.emiratesIdExpiryDate.value[0]
+        ).format('DD MMMM YYYY'),
+        residenceVisaExpiryDate: moment(
+          this.f.residenceVisaExpiryDate.value[0]
+        ).format('DD MMMM YYYY'),
+      },
+    };
+  }
+
+  /**
+   * Upload Image
+   *
+   * @param event
+   */
+  uploadFile(event: any) {
+    let inputId = event.target.id;
+    let data = null;
+    console.log('=====uploadFile=========inputId==============', inputId);
+
+    if (event.target.files && event.target.files[0]) {
+      let reader = new FileReader();
+
+      reader.onload = (event: any) => {
+        console.log(
+          '=====uploadFile===onload======inputId========event======',
+          event
+        );
+
+        data = event.target.result;
+        this.f[inputId].setValue(data);
+      };
+      reader.readAsDataURL(event.target.files[0]);
+    }
+  }
+
+  get f() {
+    return this.docsForm.controls;
   }
 
   back() {
     this._router.navigate(['kyc/homeaddress']);
-  }
-  next() {
-    this._router.navigate(['kyc/addshareholder']);
   }
 }
--- src/app/modules/kyc/components/shareholder-information/shareholder-information-page.component.ts
+++ src/app/modules/kyc/components/shareholder-information/shareholder-information-page.component.ts
@@ -1,53 +1,98 @@
-import { Component, Input, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
-import { FlatpickrOptions } from "ng2-flatpickr";
-
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+import moment from 'moment';
+import { KycService } from '../../services/kyc.service';
 
 @Component({
   selector: 'app-shareholder-information-page',
   templateUrl: './shareholder-information-page.component.html',
   styleUrls: ['./shareholder-information-page.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
-
 export class ShareholderInformationPageComponent implements OnInit {
-
-
-  public TDNameVar;
-  public TDEmailVar;
-  public selectBasicLoading = false;
-  basicDPdata;
-  public data: any;
-
-  public selectBasic = [
-    { name: 'UK' },
-    { name: 'USA' },
-    { name: 'Spain' },
-    { name: 'France' },
-    { name: 'Italy' },
-    { name: 'Australia' }
-  ];
-
-
-  public basicDateOptions: FlatpickrOptions = {
-    altInput: true
+  public dateOfBirthOptions: any = {
+    altInput: true,
+    defaultDate: ['2010-05-23'],
+    altFormat: 'j/m/Y',
   };
 
-  public birthDateOptions: FlatpickrOptions = {
-    altInput: true
-  };
+
+  public shareHolderForm: UntypedFormGroup;
+  public submitted = false;
+  public loading = false;
+
   constructor(
     private _router: Router,
-
-  ) {
-
-  }
+    private formBuilder: UntypedFormBuilder,
+    private _kycService: KycService
+  ) {}
   ngOnInit(): void {
+    this.initForm();
+
+    this.loadData();
+  }
+
+  initForm() {
+    // Reactive form initialization
+    this.shareHolderForm = this.formBuilder.group({
+      partnerShareholder: [false],
+      type: ['company'],
+      percentageHoldings: [0, Validators.required],
+      partnerName: [null, Validators.required],
+      passportNumber: [null, Validators.required],
+      phoneNumber: [null, Validators.required],
+      gender: ['male'],
+      nationality: [null, Validators.required],
+      dateOfBirth: [null, Validators.required],
+    });
+  }
+
+  loadData() {}
+
+  onSubmit() {
+    console.log('=-=-=-=-=-=-=--', this.shareHolderForm);
+
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.shareHolderForm.invalid) {
+      return;
+    }
+    this.loading = true;
+    let data = this.getCleanValue();
+
+    this._kycService.setData(data).subscribe(
+      (res) => {
+        console.log(res);
+
+        this._router.navigate(['/shuftipro/main']);
+      },
+      (err) => {
+        this.loading = false;
+        console.log(err);
+      }
+    );
+  }
+
+  getCleanValue() {
+    return {
+      partnerShareHolder: {
+        ...this.shareHolderForm.value,
+        dateOfBirth: moment(this.f.dateOfBirth.value[0]).format('DD MMMM YYYY'),
+      },
+    };
+  }
+
+  get f() {
+    return this.shareHolderForm.controls;
   }
 
   back() {
     this._router.navigate(['kyc/uploaddocuments']);
-  }
-  next() {
-    this._router.navigate(['loanbooking/loanterms']);
   }
 }
